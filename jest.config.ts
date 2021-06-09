cc22bc71b0078350831d6b60c16604e16c547ebc
daikiojm <daiking.ca2@gmail.com>
2021-06-09 21:50:23 +0900

    refactor: jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
+};
+
+export default config;
